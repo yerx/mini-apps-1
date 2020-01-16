3e25a20a6c2f5df415398cc339277aa2195acf1b
yerx <yer.x.xiong@gmail.com>
2020-01-16 15:27:02 -0500

    environment set up

---
+++ challenge_4/client/src/components/app.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return(
+      <div>
+        <h2>react working!</h2>
+      </div>
+    )
+  }
+}
+
+export default App;
---
+++ challenge_4/webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+module.exports = {
+  entry: './client/src/index.jsx',
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      }
+    ]
+  },
+  watch: true
+};
