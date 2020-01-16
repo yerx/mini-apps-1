0dd2fc9138912b4ff2d0a464a35583bfe77fd115
yerx <yer.x.xiong@gmail.com>
2020-01-15 21:07:21 -0500

    update

---
+++ challenge_3/server/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const port = 4000;
+
+// start the server at public for the index.html
+app.use(express.static('./public'));
+
+// app.get('/', (req, res) => {
+//   res.send('Hello World!')
+// })
+
+app.listen(port, () => console.log(`Listening on ${port}`));
+
+
+
+// Error: Cannot find module '/Users/yerxiong/code/yer-space/HRR43/hrr43-mini-apps-1/challenge_3/server'
+
+// /Users/yerxiong/code/yer-space/HRR43/hrr43-mini-apps-1/challenge_3/server
